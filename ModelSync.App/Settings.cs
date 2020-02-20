5dfa2171683d7328e1f8fafc86efb24e3bdcd541
Adam ONeil <adamosoftware@gmail.com>
2020-02-20 12:12:11 -0500

    renamed WinForms -> App

---
+++ ModelSync.App/Settings.cs
@@ -0,0 +1,13 @@
+﻿using JsonSettings.Library;
+using WinForms.Library.Models;
+using static System.Environment;
+
+namespace ModelSync.App
+{
+    public class Settings : SettingsBase
+    {
+        public FormPosition Position { get; set; }
+
+        public override string Filename => BuildPath(SpecialFolder.LocalApplicationData, "ModelSync", "Settings.json");
+    }
+}
---
+++ ModelSync.App/frmMain.cs
@@ -0,0 +1,44 @@
+﻿using JsonSettings.Library;
+using System;
+using System.Windows.Forms;
+using WinForms.Library.Models;
+using WinForms.Library;
+
+namespace ModelSync.App
+{
+    public partial class frmMain : Form
+    {
+        private Settings _settings;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                _settings = SettingsBase.Load<Settings>();
+                _settings.Position?.Apply(this);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                _settings.Position = FormPosition.FromForm(this);
+                _settings.Save();
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+    }
+}
