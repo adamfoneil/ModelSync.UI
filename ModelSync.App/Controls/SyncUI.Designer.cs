337de8219b9acd8d9b49bb94e51411bf35f5a0bc
Adam ONeil <adamosoftware@gmail.com>
2020-02-20 16:01:38 -0500

    fixed panel

--- ModelSync.App/Controls/SyncUI.Designer.cs
+++ ModelSync.App/Controls/SyncUI.Designer.cs
@@ -86,6 +86,7 @@
             // splitContainer1
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
             this.splitContainer1.Location = new System.Drawing.Point(0, 73);
             this.splitContainer1.Name = "splitContainer1";
             // 
