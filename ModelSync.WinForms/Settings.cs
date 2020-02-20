06a6aeb2c0c3ef41d40499160615d938d23fba41
Adam ONeil <adamosoftware@gmail.com>
2020-02-20 10:53:55 -0500

    started on this

---
+++ ModelSync.WinForms/Settings.cs
@@ -0,0 +1,12 @@
+﻿using JsonSettings.Library;
+using WinForms.Library.Models;
+
+namespace ModelSync.WinForms
+{
+    public class Settings : SettingsBase
+    {
+        public FormPosition Position { get; set; }
+
+        public override string Filename => throw new System.NotImplementedException();
+    }
+}
--- ModelSync.WinForms/frmMain.cs
+++ ModelSync.WinForms/frmMain.cs
@@ -12,9 +12,23 @@ namespace ModelSync.WinForms
 {
     public partial class frmMain : Form
     {
+        private Settings _settings;
+
         public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                //_settings = 
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
         }
     }
 }
