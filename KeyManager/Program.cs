182b9375a0eca111e1f65915db215e4ed671f952
Adam O'Neil <adamosoftware@gmail.com>
2021-03-15 11:42:02 -0400

    started on KeyManager project, but need to overhaul backend service

---
+++ KeyManager/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+
+namespace KeyManager
+{
+    class Program
+    {
+        private static HttpClient _client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            // create: create a new key for an email address (after someone purchases) and send to that person
+
+            // query: find a key belonging to an email address (help someone recover)
+
+            // verify: test validation of a key as a sanity check
+        }
+    }
+}
